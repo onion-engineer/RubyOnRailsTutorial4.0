f69e2a7acbf44d6bd9d666c95d63d6a47135e973
onion-engineer <onion.engineer@gmail.com>
2014-08-09 15:35:29 +0900

    リスト1.3 rails newを実行してアプリケーションを新規作成する。

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4536fbfc8883ca2222b0760fb523d1108c14a522a48f8e6b425e42d22a35e66a06e0876b2783400f5d8866ea82293be652ea17d765495fcff62e6ed3af0980b1'
