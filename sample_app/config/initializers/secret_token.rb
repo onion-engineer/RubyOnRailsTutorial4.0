0411ad2c11a4a2052c96175d1d7fc79fc01242de
onion-engineer <onion.engineer@gmail.com>
2014-08-10 11:18:26 +0900

    第3章 ほぼ静的なページの作成 rails new sample_app --skip-test-unit

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '4fcbecd8f78b43b0d1ebc56e8bdfb37df989d87c1ce42fb35e29f93da69aee0f5a99be09159a69f1a5e7058018db9b29684aa49372e1daefa7dd5ace0a467202'
